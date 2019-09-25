cfead8af9b0e3d38fa18c061237a66bfb3e7f974
Egor Glazkov <vtb210442@dev.vtb>
2019-09-25 09:13:55 +0300

    Updates

---
+++ precache-manifest.c76fcb7910ffadbb3d82cadf9f159a70.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f7db086aa90a4d25678f7e7461a904c",
+    "url": "/weather_reactApp/index.html"
+  },
+  {
+    "revision": "4e1080cc84adb088016b",
+    "url": "/weather_reactApp/static/css/main.5cce508b.chunk.css"
+  },
+  {
+    "revision": "01a613e0de59ef231809",
+    "url": "/weather_reactApp/static/js/2.7c6da4f7.chunk.js"
+  },
+  {
+    "revision": "4e1080cc84adb088016b",
+    "url": "/weather_reactApp/static/js/main.543cf272.chunk.js"
+  },
+  {
+    "revision": "0d0f11747e8043079d72",
+    "url": "/weather_reactApp/static/js/runtime-main.f70d9dfc.js"
+  }
+]);
